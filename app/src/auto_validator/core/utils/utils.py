7484ff70c3dab7f9793aae13b7af640e32e9d566
drunest <venus1004@proton.me>
2024-09-28 12:27:56 -0500

    fix: update utils

--- app/src/auto_validator/core/utils/utils.py
+++ app/src/auto_validator/core/utils/utils.py
@@ -5,7 +5,7 @@ import requests
 from django.conf import settings
 from django.shortcuts import redirect, render
 
-from ..models import Hotkey, Server, Subnet, ValidatorInstance
+from ..models import Subnet
 
 GITHUB_URL = settings.SUBNETS_INFO_GITHUB_URL
 
@@ -46,11 +46,10 @@ def fetch_and_compare_subnets(request):
     )
 
 
-def get_subnet_by_hotkey(hotkey_ss58, ip_address):
-    try:
-        hotkey = Hotkey.objects.get(hotkey=hotkey_ss58)
-        server = Server.objects.get(ip_address=ip_address)
-        validator = ValidatorInstance.objects.get(hotkey=hotkey, server=server)
-    except ValidatorInstance.DoesNotExist:
-        return None
-    return validator.subnet_slot.subnet
+def get_user_ip(request):
+    ip_address = request.META.get("HTTP_X_FORWARDED_FOR")
+    if ip_address:
+        ip_address = ip_address.split(",")[0]
+    else:
+        ip_address = request.META.get("REMOTE_ADDR")
+    return ip_address
